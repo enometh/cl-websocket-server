1aa3cd16355392241039afed8fb5ac0951c50bfe
Madhu <enometh@net.meer>
2025-06-22 02:01:01 +0530

    mini-clog
    [250624]
    * mini-clog.lisp: (add-to-hook): pushnew

---
+++ mini-clog.lisp
@@ -0,0 +1,141 @@
+;;; -*- Mode: LISP; Package: :cl-user; BASE: 10; Syntax: ANSI-Common-Lisp; -*-
+;;;
+;;;   Time-stamp: <>
+;;;   Touched: Sat Jun 21 20:24:46 2025 +0530 <enometh@net.meer>
+;;;   Bugs-To: enometh@net.meer
+;;;   Status: Experimental.  Do not redistribute
+;;;   Copyright (C) 2025 Madhu.  All Rights Reserved.
+;;;
+;;; with ideas from rabbibotton/clog and byulparan/websocket-demo
+;;; but without a parenscript layer.
+
+(defpackage "MINI-CLOG"
+  (:use "CL")
+  (:import-from "WEBSOCKET-SERVER" "CALL-IN-WS-REPL"))
+(in-package "MINI-CLOG")
+
+;;; ad-hoc hooks system
+(defun add-to-hook (hook-var sym &optional prepend)
+  (check-type sym symbol)
+  (assert (fboundp sym))
+  (unless (find sym (symbol-value hook-var))
+    (set hook-var
+	 (if prepend
+	     (cons sym (symbol-value hook-var))
+	     (nconc (symbol-value hook-var) (list sym))))
+  hook-var))
+
+(defun run-hooks (hook-var)
+  (loop for sym in (symbol-value hook-var)
+	do (with-simple-restart (cont "Cont")
+	     (assert (fboundp sym))
+	     (funcall sym))))
+
+(defun remove-hook (hook-var sym)
+  (set hook-var (delete sym (symbol-value hook-var))))
+
+(defvar *boot-hooks* nil)
+
+(defun boot ()
+  "Initializes the `objreg' dictionary object on the default ws
+connection."
+  (call-in-ws-repl "var boot = true;
+try {
+  if (!(objreg === undefined)) {
+    boot = false;
+  }
+} catch (error) {
+}
+if (boot) { objreg = {} }
+check_id = function (id) {
+  if (!(undefined === objreg[id])) {
+    throw `${id} already assigned to ${objreg[id].constructor.name}`
+  }
+}
+document.body.id= 'body'; objreg['body']=document.body")
+  (run-hooks '*boot-hooks*))
+
+(defun get-objreg (id)
+  ;; "defined for illustration-al purposes only"
+  (call-in-ws-repl (format nil "objreg['~A']" id)))
+
+(defun delete-element (child-id)
+  "Remove an htmlElement with id CHILD-ID from the document tree. (This
+would have been created with CREATE-ELEMENT and added to the document
+tree with APPEND-CHILD.)"
+  (call-in-ws-repl (format nil "var a = objreg['~A']; a.parentNode.removeChild(a);" child-id)))
+
+(defvar *new-id* (list 0))
+(defun generate-id ()
+  (incf (car *new-id*)))
+
+(defun create-element (tagname &optional html-id)
+  "Creates and registers an htmlElement of type TAGNAME.  Does not
+add it to the document's children."
+  (unless html-id
+    (setq html-id (format nil "G~A~A" tagname (generate-id))))
+  (call-in-ws-repl (format nil "
+var html_id = '~A';
+var tagname = '~A';
+check_id(html_id);
+objreg[html_id] = document.createElement(tagname);
+objreg[html_id].id = html_id;
+html_id"
+			   html-id tagname)))
+
+(defun get-attr (html-id attr &rest more-attrs)
+  (call-in-ws-repl (format nil "objreg['~A'].getAttribute('~A')~{?.getAttribute('~A')~}" html-id attr more-attrs)))
+
+(defsetf get-attr (html-id attr &rest more-attrs) (new-value)
+  (let* ((all-attrs (cons attr more-attrs))
+	 (tail-cons (last all-attrs))
+	 (pre-attrs (ldiff all-attrs tail-cons)))
+    `(progn (call-in-ws-repl (format nil "objreg['~A']~{.?getAttribute('~A')~}.setAttribute('~A','~A')" ,html-id ,pre-attrs ,(car tail-cons) ,new-value))
+	    ,new-value)))
+
+(defun get-prop (html-id prop &rest more-props)
+  (call-in-ws-repl (format nil "objreg['~A'].~A~{?.~A~}" html-id prop more-props)))
+
+(defsetf get-prop (html-id prop &rest more-props) (new-value)
+  (let* ((all-props (cons prop more-props))
+	 (tail-cons (last all-props))
+	 (pre-props (ldiff all-props tail-cons)))
+    `(progn (call-in-ws-repl (format nil "objreg['~A']~{.~A~}.~A= ~A" ,html-id ',pre-props ,(car tail-cons) ,new-value))
+	    ,new-value)))
+
+(defun append-child (child-id &optional parent-html-id)
+  (call-in-ws-repl (format nil "~A.appendChild(objreg['~A']);"
+			   (if parent-html-id
+			       (format nil "objreg['~A']" parent-html-id)
+			       "document.body")
+			   child-id)))
+
+#||
+(boot)
+(call-in-ws-repl "document.body.childElementCount")
+(call-in-ws-repl "document.body.removeChild(document.body.children[document.body.childElementCount - 1])")
+(setq $p (create-element "p"))
+(append-child $p)
+(setq $txt "   London. Michaelmas term lately over, and the Lord Chancellor sitting in
+    Lincoln's Inn Hall. Implacable November weather. As much mud in the streets
+    as if the waters had but newly retired from the face of the earth, and it
+    would not be wonderful to meet a Megalosaurus, forty feet long or so,
+    waddling like an elephantine lizard up Holborn Hill.")
+(setf (get-prop $p "innerHTML")
+      (concatenate 'string "'" (websocket-server::escape-string $txt) "'"))
+websocket-server:*client*
+(setq $s (create-element "script"))
+;; define a function `ty' on the webpage and call it
+(append-child $s)
+(get-prop $s "innerHTML")
+(setf (get-prop $s "innerHTML")
+      "function ty(c) {return c.constructor.name}")
+(call-in-ws-repl (format nil "ty(objreg['~a'])" $s))
+(delete-element $s)
+(setf (get-attr $s "src")
+      "/home/madhu/public_html/stage/webgl-drexel/greggman.github.io/webgl-lint/webgl-lint.js")
+(setf (get-attr $s "src") "")
+(setf (get-attr $s "src")  "/dev/shm/f1.js")
+(append-child $s)
+(call-in-ws-repl "Object.getOwnPropertyDescriptors(objreg)")
+||#
